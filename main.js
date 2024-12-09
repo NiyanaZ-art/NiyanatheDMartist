232d4d0bd27003b817652b70c97661e11ea743ee
Niyana <72097967+NiyanaZ-art@users.noreply.github.com>
2024-12-09 00:23:52 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: [ "Photographer","Graphic Designer", "Social Media Director", "Social Activitst", "Digital Media Artist"],
+    typeSpeed: 100, 
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
